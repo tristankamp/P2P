dfd5ed8869b599b586238e3f56ba5eb4f81ff72a
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2020-10-20 13:31:02 +0200

    made another project

---
+++ UserInterface/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using P2P;
+
+namespace UserInterface
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string Filename;
+            string IpAddress;
+            int Portnumber;
+            Console.WriteLine("\n Please enter the name of the file: ");
+            Filename = Console.ReadLine();
+            Console.WriteLine("\n Please enter Your Ip Address: ");
+            IpAddress = Console.ReadLine();
+            Console.WriteLine("\n Please enter Your Port Number: ");
+            Portnumber = Int32.Parse(Console.ReadLine());
+
+            
+
+        }
+    }
+}
